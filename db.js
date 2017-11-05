013261b7fb7e009ffc57f790d80706526af57f4e
humbl3man <konstantindev925@gmail.com>
2017-11-04 23:18:22 -0700

    add remote database with environment variables

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// set this to false before deploying
+const IS_DEV = true;
+
+if (!IS_DEV) {
+  const { DB_USERNAME } = process.env;
+  const { DB_PASS } = process.env;
+  mongoose.connect(`mongodb://${DB_USERNAME}:${DB_PASS}@ds249415.mlab.com:49415/todoapp`);
+} else {
+  mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
 
 mongoose.Promise = global.Promise;
 
