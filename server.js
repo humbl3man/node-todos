7fac240f6b13a8ceb5acc0534103ef48ee0d7c40
humbl3man <konstantindev925@gmail.com>
2017-11-11 22:28:35 -0800

    remove comments

--- server.js
+++ server.js
@@ -2,15 +2,6 @@ const { app } = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-// controllers
-// const { UserController } = require('./controllers/userController');
-// const { TodoController } = require('./controllers/todoController');
-
-// config
-
-// UserController(app);
-// TodoController(app);
-
 app.listen(PORT, () => {
   console.log(`start server at PORT ${PORT}`);
 });
